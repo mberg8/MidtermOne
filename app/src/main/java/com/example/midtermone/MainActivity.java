e3547d6701ab1bff9bdebd289560d1373aecd5f2
mberg8 <64221872+mberg8@users.noreply.github.com>
2021-06-02 04:35:09 -0500

    6/2/21. Margie Berg. Midterm/Final?. Completed design. Completed Main. Completed 12 Activities. Build successful.

--- app/src/main/java/com/example/midtermone/MainActivity.java
+++ app/src/main/java/com/example/midtermone/MainActivity.java
@@ -1,14 +1,111 @@
 package com.example.midtermone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_vert);
+        setContentView(R.layout.activity_main);
+
+        card1=(CardView) findViewById(R.id.cardView1);
+        card2=(CardView) findViewById(R.id.cardView2);
+        card3=(CardView) findViewById(R.id.cardView3);
+        card4=(CardView) findViewById(R.id.cardView4);
+        card5=(CardView) findViewById(R.id.cardView5);
+        card6=(CardView) findViewById(R.id.cardView6);
+        card7=(CardView) findViewById(R.id.cardView7);
+        card8=(CardView) findViewById(R.id.cardView8);
+        card9=(CardView) findViewById(R.id.cardView9);
+        card10=(CardView) findViewById(R.id.cardView10);
+        card11=(CardView) findViewById(R.id.cardView11);
+        card12=(CardView) findViewById(R.id.cardView12);
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+        card5.setOnClickListener(this);
+        card6.setOnClickListener(this);
+        card7.setOnClickListener(this);
+        card8.setOnClickListener(this);
+        card9.setOnClickListener(this);
+        card10.setOnClickListener(this);
+        card11.setOnClickListener(this);
+        card12.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.cardView1 :
+                i = new Intent(this,book1.class);
+                startActivity(i);
+                break;
+            case R.id.cardView2 :
+                 i = new Intent(this,book2.class);
+                 startActivity(i);
+                 break;
+
+            case R.id.cardView3:
+                i= new Intent(this,book3.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView4:
+                i= new Intent(this,book4.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView5:
+                i= new Intent(this,book5.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView6:
+                i= new Intent(this,book6.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView7:
+                i= new Intent(this,book7.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView8:
+                i= new Intent(this,book8.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView9:
+                i= new Intent(this,book9.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView10:
+                i= new Intent(this,book10.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView11:
+                i= new Intent(this,book11.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardView12:
+                i= new Intent(this,book12.class);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/midtermone/book1.java
@@ -0,0 +1,14 @@
+package com.example.midtermone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class book1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book1);
+    }
+}
