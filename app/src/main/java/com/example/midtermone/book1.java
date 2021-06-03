b00c6639729a24349e2995c0a96343bd1ffa633b
mberg8 <64221872+mberg8@users.noreply.github.com>
2021-06-03 02:52:45 -0500

    6/3. Updated Button functionality.

--- app/src/main/java/com/example/midtermone/book1.java
+++ app/src/main/java/com/example/midtermone/book1.java
@@ -1,23 +1,46 @@
 package com.example.midtermone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class book1 extends AppCompatActivity implements View.OnClickListener {
 
-    public Button button;
+    public Button buttonMenu;
+    public Button buttonOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book1);
+
+        buttonMenu = findViewById(R.id.button1);
+        buttonOrder = findViewById(R.id.button1_1);
+
+        buttonMenu.setOnClickListener(this);
+        buttonOrder.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v) {
-        
+        Intent i;
+        Log.d("CIS 3334", "In onClick");
+        switch (v.getId()) {
+            case R.id.button1:
+                i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                break;
+            case R.id.button1_1:
+                i = new Intent(this, orderbook.class);
+                startActivity(i);
+                break;
+
+        }
     }
 }
