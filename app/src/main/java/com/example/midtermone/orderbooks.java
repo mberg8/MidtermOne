446931f2186f434706a16c86bfe6674fbe655b35
mberg8 <64221872+mberg8@users.noreply.github.com>
2021-06-05 07:46:26 -0500

    6/5.Final Commit 7:45 AM :) Added an Order class. Finished Real Time Database to track user book orders. Confirmed functionality on Firebase Console. Total of 14 Activities including the Main.

--- app/src/main/java/com/example/midtermone/orderbooks.java
+++ app/src/main/java/com/example/midtermone/orderbooks.java
@@ -1,15 +1,21 @@
 package com.example.midtermone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 
 public class orderbooks extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,7 +25,7 @@ public class orderbooks extends AppCompatActivity implements View.OnClickListene
     FirebaseDatabase database;
     DatabaseReference myRef;
     Order order;
-    int i = 0;
+    int i;
 
 
     @Override
@@ -48,26 +54,127 @@ public class orderbooks extends AppCompatActivity implements View.OnClickListene
         checkBox11 = (CheckBox) findViewById(R.id.checkBox11);
         checkBox12= (CheckBox) findViewById(R.id.checkBox12);
 
-        checkBox1.setOnClickListener(this);
-        checkBox2.setOnClickListener(this);
-        checkBox3.setOnClickListener(this);
-        checkBox4.setOnClickListener(this);
-        checkBox5.setOnClickListener(this);
-        checkBox6.setOnClickListener(this);
-        checkBox7.setOnClickListener(this);
-        checkBox8.setOnClickListener(this);
-        checkBox9.setOnClickListener(this);
-        checkBox10.setOnClickListener(this);
-        checkBox11.setOnClickListener(this);
-        checkBox12.setOnClickListener(this);
+       String check1 = "Anne of Green Gables";
+       String check2 = "Black Beauty";
+       String check3 = "Hatchet";
+       String check4 = "The Lion the Witch and the Wardrobe";
+       String check5 = "Little Women";
+       String check6 = "Mary Poppins";
+       String check7 = "The Wizard of Oz";
+       String check8 = "Peter Pan";
+       String check9 = "Pippi LongStalking";
+       String check10 = "Where the Red Fern Grows";
+       String check11 = "The Secret Garden";
+       String check12 = "Wild Hearts Can't Be Broken";
+
+       myRef.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+               if(snapshot.exists()){
+                   i = (int)snapshot.getChildrenCount();
+               }
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
+
+        confirmOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox1.isChecked()){
+
+                    order.setAnne(check1);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox2.isChecked()){
+
+                    order.setBlackBeauty(check2);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox3.isChecked()){
+
+                    order.setHatchet(check3);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox4.isChecked()){
+
+                    order.setLionWitchWardrobe(check4);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox5.isChecked()){
+
+                    order.setLittleWomen(check5);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox6.isChecked()){
+
+                    order.setMaryPoppins(check6);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox7.isChecked()){
+
+                    order.setWizardOfOz(check7);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox8.isChecked()){
+
+                    order.setPeterPan(check8);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox9.isChecked()){
+
+                    order.setPippiLongStalking(check9);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox10.isChecked()){
+
+                    order.setWhereTheRedFernGrows(check10);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else if (checkBox11.isChecked()){
+
+                    order.setTheSecretGarden(check11);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+
+                }
+                else{
+                    order.setWildHeartsCantBeBroken(check12);
+
+                    myRef.child(String.valueOf(i+1)).setValue(order);
+                }
+            }
+        });
 
     }
 
 
-
     @Override
     public void onClick(View v) {
-        myRef.setValue(confirmOrder);
 
     }
 }
